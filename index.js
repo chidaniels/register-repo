87d8dc8d7251ba6d5d289866e2c7eb418eae9e32
chidaniel <61457861+chidaniels@users.noreply.github.com>
2023-02-28 20:41:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const firstname=document.getElementById('firstname');
-const lastname=document.getElementById('lastname');
-const phone=document.getElementById('phone');
-const email=document.getElementById('email');
-const password=document.getElementById('password');
-const form=document.getElementById('form');
-const errorElement=document.getElementById('error');
+const myForm =document.querySelector('#my-form');
+const firstName =document.querySelector('#firstname');
+const lastName =document.querySelector('#lastname');
+const phoneNo =document.querySelector('#phone');
+const emailAdd =document.querySelector('#email');
+const errorEl =document.querySelector('#error');
+const btn =document.querySelector('#button');
 
-form.addEventListener('onsubmit', (e) => {
-    let messages=[]
-    if (firstname.value === "") {
-        messages.push('this is a required field');
+myForm.addEventListener('submit', onSubmit);
 
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (firstName.value === '' || lastName.value === '') {
+        errorEl.innerText='this is a required field';
+
+        setTimeout(() => errorEl.remove(), 3000)
+    } else{
+        alert('this form has been submitted');
     }
-    if (messages.length>0) {
-        e.defaultPrevented();
-        errorElement.innerText=messages.join(',')
-    }
-});
+}
